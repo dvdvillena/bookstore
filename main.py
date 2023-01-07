d794d2ceee86141375abcf2395b96d3b893d0219
dvdvillena <dvillena14@gmail.com>
2023-01-07 23:16:09 +0100

    Initial commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 #     main.py - Startup module
 #
 ##############################################################################
-# Copyright (c) 2022, 2023, 2023 David Villena
+# Copyright (c) 2022, 2023 David Villena
 # All rights reserved.
 # Licensed under the New BSD License
 # (http://www.freebsd.org/copyright/freebsd-license.html)
